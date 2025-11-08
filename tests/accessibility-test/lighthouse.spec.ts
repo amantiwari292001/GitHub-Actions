c1250f600ece6bb46446a47259fb69967c1c3932
Aman <amantiwari2912@gmail.com>
2025-11-08 19:21:04 +0530

    New Light House way III

--- tests/accessibility-test/lighthouse.spec.ts
+++ tests/accessibility-test/lighthouse.spec.ts
@@ -1,29 +1,21 @@
-  // tests/lighthouse.spec.ts
-  import { test, expect } from '@playwright/test';
-  import { playAudit } from 'playwright-lighthouse';
+import { test, expect } from '@playwright/test';
 import { urls } from '../../data/urls/urls';
 
-  test('Lighthouse audit for homepage', async ({ page }) => {
-    await page.goto(urls.baseURL); // Replace with your application's URL
+test('Lighthouse audit for homepage', async ({ page }) => {
+  await page.goto(urls.baseURL); // Replace with your application's URL
 
-    await playAudit({
-      page,
-      thresholds: {
-        performance: 50, // Example threshold
-        accessibility: 50,
-        'best-practices': 90,
-        seo: 50,
-        pwa: 0,
-      },
-      port: 9222, // Use a free port
-      // Optional: output options for reports
-      // output: 'html',
-      // formFactor: 'mobile',
-      // screenEmulation: {
-      //   mobile: true,
-      //   width: 360,
-      //   height: 640,
-      //   deviceScaleFactor: 2,
-      // },
-    });
+  // dynamically import the ESM-only module
+  const { playAudit } = await import('playwright-lighthouse');
+
+  await playAudit({
+    page,
+    thresholds: {
+      performance: 50,
+      accessibility: 50,
+      'best-practices': 90,
+      seo: 50,
+      pwa: 0,
+    },
+    port: 9222,
   });
+});
