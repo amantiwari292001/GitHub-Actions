fba5d74429abdc491e21d7968f5108ec96bfe8ee
Aman <amantiwari2912@gmail.com>
2025-11-09 23:11:41 +0530

    Fixes

--- accessibility-test/lighthouserc.js
+++ accessibility-test/lighthouserc.js
@@ -2,19 +2,23 @@ module.exports = {
   ci: {
     collect: {
       url: [
-        'https://www.amazon.in/'
+        'https://www.youtube.com/'
       ],
       settings: {
         onlyCategories: ['accessibility', 'performance', 'seo', 'best-practices'],
+        output: ['html', 'json'],
       },
     },
     assert: {
       assertions: {
-        'categories:accessibility': ['error', { minScore: 0.82 }],
-        'categories:performance': ['error', { minScore: 0.82 }],
-        'categories:seo': ['error', { minScore: 0.82 }],
-        'categories:best-practices': ['warn', { minScore: 0.82 }],
+        'categories:accessibility': ['error', { minScore: 0.8 }],
+        'categories:performance': ['error', { minScore: 0.8 }],
+        'categories:seo': ['error', { minScore: 0.8 }],
+        'categories:best-practices': ['error', { minScore: 0.8 }],
       },
+    },
+    upload: {
+      target: '.lighthouseci/',
     },
   },
 };
