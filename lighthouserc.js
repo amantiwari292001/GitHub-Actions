18088323ae9a3f14bad517497986c84d1f847d30
Aman <amantiwari2912@gmail.com>
2025-11-09 22:34:06 +0530

    Upload artifacts-I

--- lighthouserc.js
+++ lighthouserc.js
@@ -13,14 +13,14 @@ module.exports = {
     assert: {
       assertions: {
         'categories:accessibility': ['error', { minScore: 0.9 }],
-        'categories:performance': ['warn', { minScore: 0.8 }],
-        'categories:seo': ['warn', { minScore: 0.85 }],
-        'categories:best-practices': ['warn', { minScore: 0.9 }],
+        'categories:performance': ['error', { minScore: 0.8 }],
+        'categories:seo': ['error', { minScore: 0.85 }],
+        'categories:best-practices': ['error', { minScore: 0.9 }],
         // Optional: 'categories:pwa': ['warn', { minScore: 0.5 }]
       },
     },
     upload: {
-      target: 'temporary-public-storage',
+      target: '.lighthouseci/',
     },
   },
 };
