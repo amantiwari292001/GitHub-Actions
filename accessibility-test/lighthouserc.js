3205f06067601832a53d89818c5b6db1ad7aeb01
Aman <amantiwari2912@gmail.com>
2025-11-09 23:09:13 +0530

    Convert seo to throw error

--- accessibility-test/lighthouserc.js
+++ accessibility-test/lighthouserc.js
@@ -12,7 +12,7 @@ module.exports = {
       assertions: {
         'categories:accessibility': ['error', { minScore: 0.82 }],
         'categories:performance': ['error', { minScore: 0.82 }],
-        'categories:seo': ['warn', { minScore: 0.82 }],
+        'categories:seo': ['error', { minScore: 0.82 }],
         'categories:best-practices': ['warn', { minScore: 0.82 }],
       },
     },
